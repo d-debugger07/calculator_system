18041a5e78b40a2eec7a86b7062a32664903559d
D-debugger07! <usmanabdulrahmanx007@gmail.com>
2025-10-02 09:56:31 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number: "))
+operation = input("Enter operation: ")
+num2 = int(input("Enter second number: "))
+
+if operation == "+":
+    print(f"{num1 + num2}")
+elif operation == "-":
+    print(f"{num1 - num2}")
+elif operation == "×":
+    print(f"{num1 * num2}")
+elif operation == "÷":
+    print(f"{num1 / num2}")
+else:
+    print("undefinee???")
